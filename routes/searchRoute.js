3de66d8b11150617be8b467483ebb46d038d9ccf
nith-yoga <nithe.yoga@gmail.com>
2024-11-22 00:16:51 -0500

    Fixed box art issue in search page

--- routes/searchRoute.js
+++ routes/searchRoute.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const router = express.Router();
 
 // Function to fetch box art from Giant Bomb API
-async function getGiantBombGameId(gameName) {
+async function getImageFromGiantBomb(gameName) {
     try {
         const response = await axios.get(`https://www.giantbomb.com/api/search/`, {
             params: {
@@ -15,32 +15,15 @@ async function getGiantBombGameId(gameName) {
         });
 
         if (response.data && response.data.results && response.data.results[0]) {
-            return response.data.results[0].id;
+            const game = response.data.results[0];
+            if (game.image && game.image.original_url) {
+                return game.image.original_url;
+            }
         }
 
         return null;
     } catch (error) {
         console.error('Error fetching box art from Giant Bomb:', error.message);
-        return null;
-    }
-}
-
-async function getBoxArtFromGiantBomb(gameId) {
-    try {
-        const response = await axios.get(`https://www.giantbomb.com/api/game/${gameId}/`, {
-            params: {
-                api_key: process.env.GBOMBAPI_KEY,
-                format: 'json'
-            }
-        });
-
-        if (response.data && response.data.results && response.data.results.image) {
-            return response.data.results.image.original_url;
-        }
-
-        return null;
-    } catch (error) {
-        console.error("Error fetching box art from Giant Bomb:", error.message);
         return null;
     }
 }
@@ -52,7 +35,7 @@ router.get('/search', async (req, res) => {
         const response = await axios.get(`https://api.rawg.io/api/games`, {
             params: {
                 key: process.env.RAWGAPI_KEY,
-                page_size: 10,
+                page_size: 16,
                 search: query
             }
         });
@@ -60,9 +43,7 @@ router.get('/search', async (req, res) => {
         const games = response.data.results;
 
         const gamesWithBoxArt = await Promise.all(games.map(async (game) => {
-            const giantBombGameId = await getGiantBombGameId(game.id);
-            const boxArtUrl = giantBombGameId ? await getBoxArtFromGiantBomb(giantBombGameId) : null;
-            
+            const boxArtUrl = await getImageFromGiantBomb(game.name);
             return { 
                 ...game, 
                 boxArtImageUrl: boxArtUrl 
